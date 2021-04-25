f7c7aa2d5f64b3edb9420c7213c4442eed63444f
andreia23 <andreialima.berto08@gmail.com>
2021-04-24 22:54:55 -0300

    Padrão proxy

--- src/main/java/br/edu/ifpb/padroes/api/pizzahot/proxy/PizzaHotServiceProxy.java
+++ src/main/java/br/edu/ifpb/padroes/api/pizzahot/proxy/PizzaHotServiceProxy.java
@@ -1,14 +1,29 @@
 package br.edu.ifpb.padroes.api.pizzahot.proxy;
 
-import br.edu.ifpb.padroes.api.pizzahot.PizzaHotPizza;
-
+import java.util.ArrayList;
 import java.util.List;
+
+import br.edu.ifpb.padroes.api.pizzahot.PizzaHotPizza;
+import br.edu.ifpb.padroes.api.pizzahot.PizzaHotServiceImpl;
 
 // TODO - implementar proxy de cache para evitar leitura do json a cada chamada do método
 public class PizzaHotServiceProxy implements PizzaHotService {
-    @Override
-    public List<PizzaHotPizza> getPizzas() {
-        // TODO - implementar proxy
-        return null;
-    }
+	
+	PizzaHotService pizzaHotService;
+
+	List<PizzaHotPizza> cachePizzas = new ArrayList<>();
+
+	public PizzaHotServiceProxy() {
+		this.pizzaHotService = new  PizzaHotServiceImpl();
+	}
+
+	@Override
+	public List<PizzaHotPizza> getPizzas() {
+		if (cachePizzas.isEmpty()) {
+			cachePizzas = pizzaHotService.getPizzas();
+		} else {
+			System.out.println("Retrieved list from cache.");
+		}
+		return cachePizzas;
+	}
 }
